6d87da5798e5e53acafbccdca3188cc44722483b
c!ph3r 1337 <198979692+c1ph3r-1337@users.noreply.github.com>
2025-05-29 13:17:02 +0530

    Update fuckyou.sh

--- fun/fuckyou/fuckyou.sh
+++ fun/fuckyou/fuckyou.sh
@@ -1,1 +1,47 @@
+#!/bin/bash
 
+# Define frames with slight vertical shifts for bouncing effect
+frames=(
+$'
+				          _
+				       	 / \
+				      _ |   | _
+				  _  / \|   |/ \
+				 / \|   |   |   ||\
+				|   |   |   |   | \>
+				|   |   |   |   |   \
+				| -   -   -   - |)   )
+				|                   /
+				 \                 /
+				  \               /
+				   \             /
+				    \           /
+'
+$'
+				         / \
+				        |\_/|
+				        |---|
+				        |   |
+				        |   |
+				      _ |=-=| _
+				  _  / \|   |/ \
+				 / \|   |   |   ||\
+				|   |   |   |   | \>
+				|   |   |   |   |   \
+				| -   -   -   - |)   )
+				|                   /
+				 \                 /
+				  \               /
+				   \             /
+				    \           /
+'
+)
+
+# Animation loop to simulate the bouncing
+while true; do
+    for frame in "${frames[@]}"; do
+        clear
+        echo "$frame"
+        sleep 0.5
+    done
+done
